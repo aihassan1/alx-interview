a22fee946e462bec2eb7af1b30a203922cadf71b
aihassan1 <abdosruor57@gmail.com>
2024-05-10 18:02:27 +0300

    test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,7 +5,15 @@ Minimum Operations
 
 
 def minOperations(n):
-    """min ops func"""
+    """Calculate the minimum number of operations required to reach a given number.
+
+    Args:
+        n (int): The target number.
+
+    Returns:
+        int: The minimum number of operations required.
+
+    """
     if n <= 1:
         return 0
 
