86c24487214decad20ca999532c9675e4691c8fa
aihassan1 <abdosruor57@gmail.com>
2024-06-09 17:54:26 +0300

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -23,7 +23,7 @@ function listOfCharacters (filmId) {
 async function characterNames (listOfCharacters) {
   for (const char of listOfCharacters) {
     const name = await new Promise((resolve, reject) => {
-      request(char, (err, message, body) =>{
+      request(char, (err, message, body) => {
         if (err) reject(err);
         else {
           resolve(JSON.parse(body).name);
