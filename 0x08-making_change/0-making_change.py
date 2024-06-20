5acfed0a1bcf03324e836d9dc39aecd05810c1ea
aihassan1 <abdosruor57@gmail.com>
2024-06-20 21:33:07 +0300

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,9 +11,14 @@ def makeChange(coins, total):
     min_ops = [total + 1 for total in range(total + 1)]
     min_ops[0] = 0
     coins = sorted(set(coins), reverse=True)
-    
+
     for i in range(1, total + 1):
         for coin in coins:
+            if coin > i:
+                continue
+            if i - coin == 0:
+                min_ops[i] = 1
+                break
             if i - coin >= 0:
                 min_ops[i] = min(min_ops[i], min_ops[i - coin] + 1)
 
