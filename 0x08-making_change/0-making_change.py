7e82a236cacf2f46f87605c9721bf5bc45f3f37c
aihassan1 <abdosruor57@gmail.com>
2024-06-20 21:25:21 +0300

    0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,11 +8,28 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    if coins == [] or coins == None:
+        return -1
+
+    try:
+        coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
     min_ops = [total + 1 for total in range(total + 1)]
     min_ops[0] = 0
+
+    coins.sort(reverse=True)
+
     for i in range(1, total + 1):
         for coin in coins:
             if i - coin >= 0:
                 min_ops[i] = min(min_ops[i], min_ops[i - coin] + 1)
 
     return min_ops[total] if min_ops[total] != total + 1 else -1
+
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
