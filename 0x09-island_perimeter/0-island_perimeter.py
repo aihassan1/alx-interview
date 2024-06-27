57d3e606120f5ea038992470fa54726f1443fcd0
aihassan1 <abdosruor57@gmail.com>
2024-06-28 00:44:57 +0300

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,16 +8,16 @@ def count_water_around_land(grid, x, y, land_param):
     cols = len(grid[0])
 
     if x + 1 < rows and grid[x + 1][y] == 0:
-        land_param.add((x + 1, y))
+        land_param.append((x + 1, y))
 
     if x - 1 >= 0 and grid[x - 1][y] == 0:
-        land_param.add((x - 1, y))
+        land_param.append((x - 1, y))
 
     if y + 1 < cols and grid[x][y + 1] == 0:
-        land_param.add((x, y + 1))
+        land_param.append((x, y + 1))
 
     if y - 1 >= 0 and grid[x][y - 1] == 0:
-        land_param.add((x, y - 1))
+        land_param.append((x, y - 1))
 
     return land_param
 
@@ -25,7 +25,7 @@ def count_water_around_land(grid, x, y, land_param):
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
 
-    land_param = set()
+    land_param = []
 
     for x in range(len(grid)):
         for y in range(len(grid[x])):
