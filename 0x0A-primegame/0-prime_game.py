490005bf12549cded15c8032669b925b7ab98a83
aihassan1 <abdosruor57@gmail.com>
2024-08-09 04:43:07 +0300

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def get_prime_numbs(n):
-    """function the gets the prime numbers list up to n"""
+    """Function that gets the prime numbers list up to n."""
     if n < 2:
         return []
 
@@ -8,7 +8,7 @@ def get_prime_numbs(n):
     nums_list[0] = nums_list[1] = False
 
     for i in range(2, int(n**0.5) + 1):
-        if nums_list[i] is True:
+        if nums_list[i]:
             for j in range(i * i, n + 1, i):
                 nums_list[j] = False
 
@@ -18,26 +18,23 @@ def get_prime_numbs(n):
 
 
 def isWinner(x, nums):
-    """define who is winner in a game"""
+    """Define who is winner in a game."""
     if x is None or nums is None or x == 0 or nums == []:
         return None
 
-    Maria_score = 0
-    Ben_Score = 0
+    maria_score = 0
+    ben_score = 0
 
     for number in nums:
         list_of_prime_nums = get_prime_numbs(number)
         moves = len(list_of_prime_nums)
         if moves % 2 == 0:
-            Ben_Score += 1
+            ben_score += 1
         else:
-            Maria_score += 1
+            maria_score += 1
 
-    if Maria_score > Ben_Score:
+    if maria_score > ben_score:
         return "Maria"
-
-    elif Maria_score < Ben_Score:
+    elif maria_score < ben_score:
         return "Ben"
-
-    else:
-        return None
+    return None
